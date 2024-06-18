81b4f10439e49e53ad4770f6f936f04ef12b53e0
Victor Rosca <58151095+Victor119@users.noreply.github.com>
2024-06-18 11:31:56 +0300

    Add files via upload

---
+++ static/scripts/main.js
@@ -0,0 +1,18 @@
+let header = document.querySelector('header');
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY>0);
+});
+
+menu.onclick= () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onroll=() =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
