54660b4e458fa7acb2b8a0d2379d0fd5ae79c058
Victor Rosca <58151095+Victor119@users.noreply.github.com>
2024-06-24 12:02:24 +0300

    Add files via upload

---
+++ static/scripts/admin/modal.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var openModalBtn = document.getElementById("btnNew");
+    var modal = document.getElementById("myModal");
+    var closeBtn = document.getElementsByClassName("close")[0];
+
+    // Ascunde modalul la început
+    modal.style.display = "none";
+
+    openModalBtn.addEventListener("click", function() {
+        modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
